4f202553446cfe112b37b07581ae42f395b9f434
Thomas Woroniak <thomasworoniak@Thomass-MacBook-Air.local>
2026-01-21 00:29:51 -0600

    add hover transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import VisitList from "./components/VisitList";
+import VisitList2 from "./components/VisitList2";
 
 const App = () => {
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center'>
-      <VisitList />
+      <VisitList2 />
     </div>
   );
 };
